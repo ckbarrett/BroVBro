3e25cb662f88a754d66aff26a1d7e3884461cb3a
Christian Barrett <ckbarrett116@gmail.com>
2026-05-10 13:28:02 -0500

    Add to allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["brovbro.ckbarrett.com"],
+  },
+  preview: {
+    allowedHosts: ["brovbro.ckbarrett.com"],
+  },
 });
